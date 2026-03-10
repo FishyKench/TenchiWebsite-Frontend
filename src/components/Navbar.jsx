be9b811da05a196ae0811ecb2a188057a82bd7a0
Mohammed Alhammaly <m7md.al7male@gmail.com>
2026-03-10 15:03:00 +0300

    styled the nav a bit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
-function Navbar(){
-    return(
-        <nav>
-            <h1>TenchiWebsite</h1>
-            <ul>
-                <li>Home</li>
-                <li>Login</li>
-            </ul>
-        </nav>
-    )
+function Navbar() {
+  return (
+    <nav className="flex items-center justify-between px-8 py-4 bg-gray-900 text-white">
+      <h1 className="text-xl font-bold">TenchiWebsite</h1>
+      <ul className="flex gap-6">
+        <li className="cursor-pointer hover:text-gray-300">Home</li>
+        <li className="cursor-pointer hover:text-gray-300">Login</li>
+      </ul>
+    </nav>
+  )
 }
+
 export default Navbar
