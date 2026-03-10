82e66bd899e5cd12f91573d103025b38738bfccf
Mohammed Alhammaly <m7md.al7male@gmail.com>
2026-03-10 15:02:11 +0300

    added tailwind

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar(){
+    return(
+        <nav>
+            <h1>TenchiWebsite</h1>
+            <ul>
+                <li>Home</li>
+                <li>Login</li>
+            </ul>
+        </nav>
+    )
+}
+export default Navbar
